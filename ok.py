bab21e43ba4e5c042af9f28559bcece1d03819b5
jonathan <jonathan.bergers@gmail.com>
2016-04-08 15:19:03 +0200

    final commit

--- ok.py
+++
@@ -1,6 +0,0 @@
-import jsonlines
-f = jsonlines.open('reviews_csm2.jsonl', 'w')
-
-for i in jsonlines.open('reviews_csm.jsonl'):
-    i['rating'] = int(i['rating']) * 2
-    jsonlines.Writer.dump(f, i)
